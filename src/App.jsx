9cdd73b509b2656c05e5f9ea10cddf0bc55efef9
Kyle Knaggs <kyleknaggs@gmail.com>
2020-11-16 13:37:37 -0400

    Remove styles loaded in by App.css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <p>Take Home Template App</p>
       </header>
     </div>
