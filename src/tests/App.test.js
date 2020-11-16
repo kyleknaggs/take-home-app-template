4fcb31cb9ac3e33dbc82ed9ab18ff5f262e5c908
Kyle Knaggs <kyleknaggs@gmail.com>
2020-11-16 15:29:07 -0400

    Move tests to own directory.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText("Take Home Template App");
+  const linkElement = screen.getByText('Take Home Template App');
   expect(linkElement).toBeInTheDocument();
 });
